5f9590e46f1c0aa6e8432413c7e489eb49e091ae
prathik402 <73998548+prathik402@users.noreply.github.com>
2021-06-03 14:00:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 var tree;
+var stone;
 var ground;
 
 function preload()
@@ -21,7 +22,8 @@ function setup() {
 	//Create the Bodies Here.
 	
     tree = new Tree1(10,10,10,10);
-    ground = new Ground(60,60,100,10)
+    ground = new Ground(60,60,100,10);
+    stone = new Stone1(10,10,10,10);
 
 	Engine.run(engine);
   
@@ -33,6 +35,7 @@ function draw() {
   background(0);
   tree.display();
   ground.display();
+  stone.display();
   
   drawSprites();
  
